aa658879fe7959fd9bada2eb522c35eb83fbe249
Shyam Kumar <shyamkumar@berkeley.edu>
2021-09-24 17:23:59 -0700

    Initial Commit

---
+++ Ranker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ranker
+//
+//  Created by Shyam Kumar on 9/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
