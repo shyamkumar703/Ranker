0bc488e13427a91ac7ac49283f8dbfe86b258981
Shyam Kumar <shyamkumar@berkeley.edu>
2022-01-11 18:09:13 -0800

    profile page

--- Ranker/ProfileViewController.swift
+++ Ranker/ProfileViewController.swift
@@ -5,16 +5,31 @@
 //  Created by Shyam Kumar on 1/10/22.
 //
 
+import FirebaseFirestore
 import UIKit
-
-// 60 height for bar chart
 
 class ProfileViewController: UIViewController {
     
-    lazy var barChart: BarChart = {
-        let chart = BarChart()
-        chart.translatesAutoresizingMaskIntoConstraints = false
-        return chart
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .regularFont.withSize(18)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontSizeToFitWidth = true
+        label.text = "My polls"
+        return label
+    }()
+    
+    lazy var table: FeedTableView = {
+        let table = FeedTableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        let db = Firestore.firestore()
+        db.getAll(collectionName: .polls, decodeInto: [Poll.self], completion: { polls in
+            if let polls = polls {
+                table.model = FeedTableViewModel(polls: polls.filter({ $0.getPollStatus() == .posted }))
+            }
+        })
+        return table
     }()
 
     override func viewDidLoad() {
@@ -25,13 +40,18 @@ class ProfileViewController: UIViewController {
     
     func setupView() {
         view.backgroundColor = .white
-        view.addSubview(barChart)
+        view.addSubview(titleLabel)
+        view.addSubview(table)
     }
     
     func setupConstraints() {
-        barChart.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        barChart.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        barChart.heightAnchor.constraint(equalToConstant: 60).isActive = true
-        barChart.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 32).isActive = true
+        titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8).isActive = true
+        titleLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -8).isActive = true
+        table.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12).isActive = true
+        table.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        table.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        table.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
     }
 }
